ecfb3efa9b975c6e2a71e47268f1a794473556e1
carlosweb <kaduinfo@gmail.com>
2020-07-14 17:17:12 -0300

    first Commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// let valorDoFrete = 50.00
+// let valorDoProduto = 60
+// let valorTotal = 0
+
+
+// if(typeof valorDoProduto === 'number' && typeof valorDoFrete === 'number'){
+//     valorTotal = valorDoProduto + valorDoFrete
+// }
+// console.log(valorTotal)
+
+
+window.addEventListener("load", function() {
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+  function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+		var anchorReference = tabClickEvent.target;
+		var activePaneId = anchorReference.getAttribute("href");
+		var activePane = document.querySelector(activePaneId);
+		activePane.classList.add("active");
+	}
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+});
